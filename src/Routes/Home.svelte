15097f1e1a167e981a5f48d210edec4a7b5b912e
Ethan <yongseok993@gmail.com>
2023-05-07 19:28:50 +0900

    영화 검색 프로젝트 기본 구조 작성

---
+++ src/Routes/Home.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Headline from '~/components/Headline.svelte'
+    import Search from '~/components/Search.svelte'
+</script>
+
+<div class="container">
+    <Headline />
+    <Search />
+</div>
+
+<style lang="scss">
+
+</style>
